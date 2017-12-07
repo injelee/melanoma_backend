8e22c95f9be86cc30cf3453f1ef75dc6320a0873
jl3392 <jingli3392@gmail.com>
2017-12-07 14:08:52 -0500

    try to modify the input format.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, make_response, json
+from find_melanoma import Melanoma
 import base64
 
 app = Flask(__name__)
@@ -7,24 +8,24 @@ app = Flask(__name__)
 @app.route("/image_classified_result", methods=['GET', 'POST'])
 def image_classified_result():
     if request.method == 'POST':
-        data = request.json
+        data = request.form
 
-        for k in data.keys():
-            encodeimage = data[k]
+    for key in data.keys():
+        for value in data.getlist(key):
+            encodeimage = value
             image_string = base64.decodestring(encodeimage)
-            image_result = open(data.keys[k]+'decode_image.jpg', 'wb')
+            image_result = open(key+'decode_image.jpg', 'wb')
             image_result.write(image_string)
 
 
     # inje's class which has tensorflow
-        # classification  = get_prediction(image_result)
+        prediction = Melanoma(image=image_result)
 
     if request.method == 'GET':
         response = make_response('image1decode_image.jpg')  # get image
         response.headers['Content-Type'] = 'image/jpeg'
         response.headers['Content-Disposition'] = 'attachment; filename = img.jpg'
-
-        #response.headers['Classification result'] = classification
+        response.headers['Classification result'] = prediction
         return response
 
 

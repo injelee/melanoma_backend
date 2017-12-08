35b6883fb7ab4f030f40fa8ac1496920e2bae98e
jl3392 <jingli3392@gmail.com>
2017-12-08 10:43:17 -0500

    delete the useless code.

--- server.py
+++ server.py
@@ -28,19 +28,5 @@ def patient_result():
         response.headers['Classification result'] = prediction
         return response
 
-def encode_image_string(filename):
-    with open(filename, “rb”) as image_file:
-        image_string = base64.b64encode(image_file.read())
-        return image_string
-
-def save_image_string(base64image, filename):
-    with open(filename, “wb”) as image_out:
-        image_out.write(base64.b64decode(base64image))
-
-
-if __name__ == ‘__main__‘:
-    encoded_image = encode_image_string(“pup.jpg”)
-    print(encoded_image)
-    save_image_string(encoded_image, “pupper_new.jpg”)
 
 
